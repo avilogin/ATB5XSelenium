1e25195c5b742901f59c6addfc0d857ab7004dfd
avijeet <avijeetjana26@gmail.com>
2024-03-21 01:13:39 +0530

    adding changes

--- src/test/java/org/example/Selenium0603/Sel12.java
+++ src/test/java/org/example/Selenium0603/Sel12.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Sel11 {
-    String expt_err_msg = "Your email, password, IP address or location did not match";
+public class Sel12 {
+    //Positive Scenario
+    String expt_page_name = "Dashboard";
 
     @Test
     @Description("Verfiy the current URl, title of VWO app")
@@ -20,22 +20,9 @@ public class Sel11 {
         driver.manage().window().maximize();
         driver.findElement(By.id("login-username")).sendKeys("avijeetjana26@gmail.com");
         WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("Password");
+        password.sendKeys("Password@2024#1");
         driver.findElement(By.id("js-login-btn")).click();
-        Thread.sleep(3000);
-        WebElement error_message = driver.findElement(By.className("notification-box-description"));
-        String error_msg_text = error_message.getText();
-        Assert.assertEquals(error_msg_text,expt_err_msg);
-
         driver.quit();
-
 
     }
 }
-
-/*
-Priority of the attribute to find the element
-id, className, name, css Selector, xpath
- LinkText and PartialText which are only for <a>
-
- */
